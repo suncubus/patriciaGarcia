509b6c6a54d9e99b93728f1b4d9c8a7624b264f3
Patri <witty.free@gmail.com>
2021-10-20 10:40:33 +0200

    Crear estructura de gallery

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Meta, MetaDefinition } from '@angular/platform-browser';
-
+declare var $:any;
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
@@ -12,6 +12,29 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        console.log(value)
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+      $(this).removeClass("active");
+    }
+      $(this).addClass("active");
   }
 
+ 
 }
